500cbc972eb876dd5ecd4e502a53930bd0002db0
Cosmin Mihu <cosmin.mihu@gmail.com>
2025-05-20 15:24:02 +0300

    Upgrade SqlDelight to version 2.0.0
    The `SqlDriver.execute` method now requires a `CoroutineContext` which is incompatible with the `create` method from `SqlSchema`. To address this, the `SqlSchema.synchronous()` method is used instead.

--- library/src/desktopMain/kotlin/ro/cosminmihu/ktor/monitor/db/DatabaseDriverFactory.kt
+++ library/src/desktopMain/kotlin/ro/cosminmihu/ktor/monitor/db/DatabaseDriverFactory.kt
@@ -1,5 +1,6 @@
 package ro.cosminmihu.ktor.monitor.db
 
+import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import ro.cosminmihu.ktor.monitor.db.sqldelight.LibraryDatabase
@@ -8,6 +9,6 @@ import java.io.File
 internal actual fun createDatabaseDriver(): SqlDriver {
     val filePath = File(System.getProperty("java.io.tmpdir"), DATABASE_NAME)
     return JdbcSqliteDriver("jdbc:sqlite:$filePath").also {
-        LibraryDatabase.Schema.create(it)
+        LibraryDatabase.Schema.synchronous().create(it)
     }
 }
